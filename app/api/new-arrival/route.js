99460d0515717160393f42af16821f9df8e91478
masud040 <masudinfo962@gmail.com>
2024-11-24 16:25:41 +0600

    remove api route

--- app/api/new-arrival/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const res = await fetch("/data/productData.json");
-  const data = await res.json();
-  return {
-    body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" },
-  };
-}
